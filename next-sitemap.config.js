095ba58eef02318447534d911bc12844e59f6db7
Daniel Erazo <danielmauricioerazoespinoza@gmail.com>
2025-07-09 21:51:41 -0500

    Sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://scriptioo.com',
+  generateRobotsTxt: true, // Generate robots.txt file
+  // You can add more options here if needed
+}; 
